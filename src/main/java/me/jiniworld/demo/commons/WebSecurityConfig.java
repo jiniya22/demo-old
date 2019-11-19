2a17885b0813d0d60a7c371c47ac1eb0cee40e9e
jini <yeonjini2222@gmail.com>
2019-11-19 11:37:46 +0900

    spring security - login page custom

---
+++ src/main/java/me/jiniworld/demo/commons/WebSecurityConfig.java
@@ -0,0 +1,34 @@
+package me.jiniworld.demo.commons;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+
+@EnableGlobalMethodSecurity(prePostEnabled=true)
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig  extends WebSecurityConfigurerAdapter {
+	
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring().antMatchers("/static/**");
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+			.antMatchers("/").permitAll()
+			.antMatchers("/v", "/v/**").access("hasRole('ROLE_VIEW')")
+		.and()
+			.formLogin().loginPage("/login").permitAll()
+			.usernameParameter("username").passwordParameter("password")
+		.and()
+			.logout().permitAll()		
+		.and()
+			.csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+	}
+}
--- src/main/java/me/jiniworld/demo/controllers/views/LoginController.java
+++ src/main/java/me/jiniworld/demo/controllers/views/LoginController.java
@@ -5,18 +5,29 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class LoginController {
 	
-	@GetMapping(value = "")
-	public String login(@AuthenticationPrincipal User user){
+	@RequestMapping(value = "")
+	public String index(@AuthenticationPrincipal User user){
 		if(user != null) {
 			if(user.getAuthorities().contains(new SimpleGrantedAuthority("ROLE_VIEW"))) {
 				return "redirect:/v";
 			}
 		}
 		return "redirect:/login";
+	}
+	
+	@GetMapping(value = "/login")
+	public String login(@AuthenticationPrincipal User user){
+		if(user != null) {
+			if(user.getAuthorities().contains(new SimpleGrantedAuthority("ROLE_VIEW"))) {
+				return "redirect:/v";
+			}
+		}
+		return "login/login";
 	}
 
 //	@GetMapping(value = "/login/access-denied")
