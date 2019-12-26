f942a2a096dabc752f5862168c7c601706e71aec
jini <yeonjini2222@gmail.com>
2019-12-26 18:08:44 +0900

    -

--- src/main/java/me/jiniworld/demo/controllers/views/LoginController.java
+++ src/main/java/me/jiniworld/demo/controllers/views/LoginController.java
@@ -1,12 +1,9 @@
 package me.jiniworld.demo.controllers.views;
 
-import javax.servlet.http.HttpSession;
-
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
