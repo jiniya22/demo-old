85ff00ced77641b7d61ac8b64b6c9a859744ab26
jini <yeonjini2222@gmail.com>
2020-05-28 11:39:11 +0900

    demo.yml test api

--- src/main/java/me/jiniworld/demo/controllers/TestController.java
+++ src/main/java/me/jiniworld/demo/controllers/TestController.java
@@ -1,9 +1,6 @@
 package me.jiniworld.demo.controllers;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Optional;
-
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,70 +9,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
-import me.jiniworld.demo.models.entities.Store;
-import me.jiniworld.demo.models.entities.User;
-import me.jiniworld.demo.services.StoreService;
-import me.jiniworld.demo.services.UserService;
+import me.jiniworld.demo.configs.TestConfig;
 
 @RequestMapping(value = "/test", produces = {MediaType.APPLICATION_JSON_VALUE})
 @RequiredArgsConstructor
 @RestController
 public class TestController {
 	
-	private final StoreService storeService;
-	private final UserService userService;
-	private final PasswordEncoder passwordEncoder;	
-	
-	@GetMapping("/users/{id}")
-	public Map<String, Object> selectUser(@PathVariable("id") long id) {
-		Map<String, Object> response = new HashMap<>();
-		
-		Optional<User> oUser = userService.findByIdUsingMapper(id);
-		
-		if(oUser.isPresent()) {
-			response.put("user", oUser);
-			response.put("result", "SUCCESS");
-		} else {
-			response.put("result", "FAIL");
-		}
-		
-		return response;
-	}
-	
-	@GetMapping("/users/{id}/2")
-	public Map<String, Object> selectUser2(@PathVariable("id") long id) {
-		Map<String, Object> response = new HashMap<>();
-		
-		Optional<User> oUser = userService.findByIdUsingMapper2(id);
-		
-		if(oUser.isPresent()) {
-			response.put("user", oUser);
-			response.put("result", "SUCCESS");
-		} else {
-			response.put("result", "FAIL");
-		}
-		
-		return response;
-	}
-	
-	@GetMapping("/stores/{id}")
-	public Map<String, Object> selectStore(@PathVariable("id") long id) {
-		Map<String, Object> response = new HashMap<>();
-		
-		Optional<Store> oStore = storeService.findByIdUsingMapper(id);
-		
-		if(oStore.isPresent()) {
-			response.put("store", oStore.get());
-			response.put("result", "SUCCESS");
-		} else {
-			response.put("result", "FAIL");
-		}
-		
-		return response;
-	}
+	private final PasswordEncoder passwordEncoder;
+	private final TestConfig testConfig;
 	
 	@GetMapping("/encode/{password}")
 	public String selectStore(@PathVariable("password") String password) {
 		return passwordEncoder.encode(password);
+	}
+	
+	@GetMapping("/config")
+	public Object configMail(@Value("${demo.api}") String demoApi) {
+		return String.format("%s\n%s", demoApi, testConfig.toString());
 	}
 }
