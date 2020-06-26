386addf6faeed509a9d2199334bc721d0d57f5e1
jini <yeonjini2222@gmail.com>
2020-06-26 10:01:41 +0900

    forward ip test

--- src/main/java/me/jiniworld/demo/controllers/TestController.java
+++ src/main/java/me/jiniworld/demo/controllers/TestController.java
@@ -1,6 +1,9 @@
 package me.jiniworld.demo.controllers;
 
+import java.util.HashMap;
 import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -62,6 +65,23 @@ public class TestController {
 		return map;
 	}
 	
+	@GetMapping(value = "/tttt")
+	public Map<String, Object> tttt(HttpServletRequest request) throws Exception {
+		Map<String, Object> res = new HashMap<>();
+		res.put("serverName", request.getServerName());
+		res.put("servletPath", request.getServletPath());
+		res.put("requestURL", request.getRequestURL());
+		res.put("requestURI", request.getRequestURI());
+		res.put("requestURI", request.getRemoteAddr());	
+		res.put("X-Forwarded-For", request.getHeader("X-Forwarded-For"));
+		res.put("X-Forwarded-Host", request.getHeader("X-Forwarded-Host"));
+		res.put("X-Forwarded-Protocol", request.getHeader("X-Forwarded-Protocol"));
+		res.put("Proxy-Client-IP", request.getHeader("Proxy-Client-IP"));
+		res.put("WL-Proxy-Client-IP", request.getHeader("WL-Proxy-Client-IP"));
+		res.put("HTTP_CLIENT_IP", request.getHeader("HTTP_CLIENT_IP"));
+		res.put("HTTP_X_FORWARDED_FOR", request.getHeader("HTTP_X_FORWARDED_FOR"));
+		return res;
+	}
 	
 	@Value("${demo.api}")
     public void setKEY(String demoApi) {
