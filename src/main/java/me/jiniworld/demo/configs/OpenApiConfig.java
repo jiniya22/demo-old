377650c9fa9023b733d4ddee3835a435cb1d8992
jini <yeonjini2222@gmail.com>
2020-06-29 10:33:59 +0900

    profile 설정

--- src/main/java/me/jiniworld/demo/configs/OpenApiConfig.java
+++ src/main/java/me/jiniworld/demo/configs/OpenApiConfig.java
@@ -1,5 +1,8 @@
 package me.jiniworld.demo.configs;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
@@ -9,21 +12,25 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.servers.Server;
 
 @Component
 public class OpenApiConfig {
 
 	@Bean
-	public OpenAPI openAPI(@Value("${springdoc.version}") String appVersion) {
-		Info info = new Info().title("Demo API").version(appVersion)
+	public OpenAPI openAPI(@Value("${demo.version}") String appVersion,
+			@Value("${demo.url}") String url, @Value("${spring.profiles.active}") String active) {
+		Info info = new Info().title("Demo API - " + active).version(appVersion)
 				.description("Spring Boot를 이용한 Demo 웹 애플리케이션 API입니다.")
 				.termsOfService("http://swagger.io/terms/")
 				.contact(new Contact().name("jini").url("https://blog.jiniworld.me/").email("jini@jiniworld.me"))
 				.license(new License().name("Apache License Version 2.0").url("http://www.apache.org/licenses/LICENSE-2.0"));
 		
+		List<Server> servers = Arrays.asList(new Server().url(url).description("demo (" + active +")"));
 		return new OpenAPI()
 				.components(new Components())
-				.info(info);
+				.info(info)
+				.servers(servers);
 	}
 	
 }
