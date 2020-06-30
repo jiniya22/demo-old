ca16f9644434a8d195d3be94a8255041e5a5b645
jini <yeonjini2222@gmail.com>
2020-06-30 09:43:11 +0900

    불필요한 메서드 제거

--- src/main/java/me/jiniworld/demo/controllers/TestController.java
+++ src/main/java/me/jiniworld/demo/controllers/TestController.java
@@ -7,13 +7,8 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -30,39 +25,14 @@ public class TestController {
 	private String demoApi;
 	
 
-	@PostMapping(value="/ddd")
-	public Map<String, String> ddd(@RequestBody Map<String, String> map) {
-		return map;
-	}
-	
-	@PatchMapping(value="/baba")
-	public Map<String, String> baba(@RequestBody Map<String, String> map) {
-		return map;
-	}
-	
-	@PutMapping(value="/abab")
-	public Map<String, String> abab(@RequestBody Map<String, String> map) {
-		return map;
-	}	
-	
 	@GetMapping(value = "/encode/{password}", produces = "text/plain")
 	public String selectStore(@PathVariable("password") String password) {
 		return passwordEncoder.encode(password);
 	}
 	
-	@PostMapping(value="/aaa")
-	public Map<String, String> aaa(@RequestBody Map<String, String> map) {
-		return map;
-	}
-	
 	@GetMapping(value="/config", produces = "text/plain")
 	public Object testConfig() {
 		return String.format("%s\n%s", demoApi, testConfig);
-	}
-	
-	@DeleteMapping(value="/bbb")
-	public Map<String, String> bbb(@RequestBody Map<String, String> map) {
-		return map;
 	}
 	
 	@GetMapping(value = "/tttt")
