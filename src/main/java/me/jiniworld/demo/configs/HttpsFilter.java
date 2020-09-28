907a8564872d93deef6b8a019efdd9d3687fbe70
jini <yeonjini2222@gmail.com>
2020-09-28 17:02:07 +0900

    -

--- src/main/java/me/jiniworld/demo/configs/HttpsFilter.java
+++ src/main/java/me/jiniworld/demo/configs/HttpsFilter.java
@@ -12,14 +12,14 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Component;
 
 @Component
-public class AuthorizationFilter implements Filter {
+public class HttpsFilter implements Filter {
 
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
 		HttpServletResponse res = (HttpServletResponse) response;
 		res.setHeader("Access-Control-Allow-Origin", "*");
-		res.setHeader("Access-Control-Allow-Methods", "POST, GET, DELETE, PUT, OPTION, PATCH");
+		res.setHeader("Access-Control-Allow-Methods", "POST, GET, DELETE, PUT, PATCH");
 		res.setHeader("Access-Control-Max-Age", "3600");
 		res.setHeader("Access-Control-Allow-Headers", "X-Requested-With, Origin, Content-Type, Accept");
         chain.doFilter(request, response);
